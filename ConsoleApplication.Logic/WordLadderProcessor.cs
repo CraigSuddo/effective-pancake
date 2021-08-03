84e35dcc9158d9533d416f5c79bd79529c687eaf
Craig Suddo <craigsuddo@gmail.com>
2021-08-03 20:55:36 +0100

    Having a mare with this word ladder logic. I have tried numerous things and I know what i want to achieve I just cant think of an elegant way to do it.

--- ConsoleApplication.Logic/WordLadderProcessor.cs
+++ ConsoleApplication.Logic/WordLadderProcessor.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication.Logic
         private IWordLadderProcessorConfiguration _config { get; set; }
         public IWordLadderProcessorResult Result { get; set; }
 
+        private List<Dictionary<int, string>> Solutions { get; set; }
+
         private List<Word> Words { get; set; }
 
         public WordLadderProcessor(IWordLadderProcessorConfiguration config)
@@ -35,15 +37,20 @@ namespace ConsoleApplication.Logic
                 {
                     foreach (var template in word.Templates)
                     {
-                        var matchingTemplateWord = Words.Where(w => w.Value != word.Value)
+                        var matchingTemplateWords = Words.Where(w => w.Value != word.Value)
                                                         .Where(w => w.Templates.Contains(template))
                                                         .ToList();
 
-                        word.ConnectedWords = matchingTemplateWord;                        
+                        word.ConnectedWords = matchingTemplateWords;                 
                     }    
                 }
 
                 // Find all the potential solutions.
+
+                // Trying something new - the solution is to make word chains, so instead what I'll do is work every potential chain through from start to a dead end.
+                var start = Words.FirstOrDefault(w => w.Value == _config.StartWord);
+                CheckForSolution(start, null);
+
 
 
                 // Return the solution(s) with the least steps - I anticipate that occasionally there may be more than 1 solution from Start - End
@@ -58,5 +65,36 @@ namespace ConsoleApplication.Logic
             return Result;
         }
 
+
+        //TODO: Something is really wrong with my logic. I'm finding connected words, but not really progressing through the tree I have.
+        // Also if a connected word has multiple connections, then I could end up going down the same routes over and over.
+        // When I find a dead end, I could add these to a list of dead ends so that I avoid them in future.
+        private void CheckForSolution(Word word, Word parent)
+        {
+            var parentValue = parent == null ? string.Empty : parent.Value;
+            foreach (var connectedWord in word.ConnectedWords.Where(cw => cw.Value != parentValue))
+            {
+                if (connectedWord.Value == _config.EndWord)
+                {
+                    // Solution found!
+                    return;
+                }
+                else
+                {
+                    CheckForSolution(connectedWord, parent);
+                }
+            }
+        }
+
+        private void AddSolution(List<string> ancestors)
+        {
+            var dict = new Dictionary<int, string>();
+            for (var i = 0; i< ancestors.Count; i++)
+            {
+                dict.Add(i, ancestors[i]);
+            }
+
+            Solutions.Add(dict);
+        }
     }
 }
--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -20,26 +20,3 @@ namespace ConsoleApplication
         }
     }
 }
-
-//Your program should calculate the shortest list of four letter words, starting with StartWord, and ending with EndWord, with a number of intermediate words that must appear in the DictionaryFile file where each word differs from the previous word by one letter only.The result should be written to the destination specified by the ResultFile argument.
-
-//For example, if StartWord = Spin, EndWord = Spot and DictionaryFile file contains
-//Spin
-//Spit
-//Spat
-//Spot
-//Span
-
-//then ResultFile should contain
-//Spin
-//Spit
-//Spot
-
-//Two examples of incorrect results:
-//Spin, Span, Spat, Spot(incorrect as it takes 3 changes rather than 2)
-//Spin, Spon, Spot(incorrect as spon is not a word)
-
-//Your solution should deal with the case where the dictionary file is not in alphabetical order.
-
-//Please explain the thought/development process you went through to achieve your solution.Your solution should be both elegant and maintainable.  Extra points awarded for solutions that have an emphasis on how the code will be tested, and performance implications.
-
